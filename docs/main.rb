331e9d84121bf0172c6979c018f4ca6c424fe267
getty104 <hayabusatoshihumi@gmail.com>
2023-06-18 10:22:46 +0900

    Refactor to use ruby-wasm-vdom

--- docs/main.rb
+++ docs/main.rb
@@ -1,23 +1,50 @@
 state = {
-  url_name: '',
+  code: '>+++++++++[<++++++++>-]<.>+++++++[<++++>-]<+.+++++++..+++.>>>++++++++[<++++>-]<.>>>++++++++++[<+++++++++>-]<---.<<<<.+++.------.--------.>>+.>++++++++++.',
+  output: ''
 }
 
 actions = {
-  update_url_name: ->(state, value) { state[:url_name] = value }
+  update_code: -> (state, value) {
+    state[:code] = value
+  },
+  run_code: ->(state, e) {
+    e.preventDefault()
+    interpreter = BrainfuckInterpreter.new
+    output = interpreter.run(state[:code])
+    state[:output] = output
+  }
 }
 
 view = ->(state, actions) {
-  isvalid = state[:url_name].length >= 4
-
-  h(:form, { class: "w-full max-w-sm" }, [
-    h(:label, { class: "block mb-2 text-sm font-medium text-700 dark:text-500" }, ['ユーザー名']),
-    h(:input, {
-      type: 'text',
-      class: isvalid ? "bg-green-50 border border-green-500 text-green-900 placeholder-green-700 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-green-100 dark:border-green-400" : "bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5 dark:bg-red-100 dark:border-red-400",
-      oninput: ->(e) { actions[:update_url_name].call(state, e[:target][:value].to_s) }
-    }, []),
-    h(:p, { class: isvalid ? "mt-2 text-sm text-green-600 dark:text-green-500" : "mt-2 text-sm text-red-600 dark:text-red-500" }, [isvalid ? "有効です" : "ユーザー名は4文字以上にしてください"])
-  ])
+  eval DomParser.parse(<<-DOM)
+    <div class="max-w-lg mx-auto bg-white p-4 rounded-lg shadow">
+      <h1 class="text-2xl font-bold mb-4">Brainf*ck Interpreter</h1>
+      <form onsubmit='{->(e) { actions[:run_code].call(state, e) }}'>
+        <div class="code-container">
+          <label for="code-input" class="block font-medium">Code:</label>
+          <textarea
+            id="code-input"
+            class="code-input border border-gray-300 rounded-lg p-2 w-full h-40"
+            oninput='{->(e) { actions[:update_code].call(state, e[:target][:value].to_s) }}'
+          >
+            {state[:code]}
+          </textarea>
+        </div>
+        <div class="mt-4">
+          <button
+            type="submit"
+            class="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
+          >Execute</button>
+        </div>
+      </form>
+      <div class="mt-8">
+        <h2 class="text-lg font-bold mb-4">Result:</h2>
+        <div class="result-container">
+          {state[:output]}
+        </div>
+      </div>
+    </div>
+  DOM
 }
 
 App.new(
